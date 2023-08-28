3b7a6081237243a5a3b60199443e2be08a2c88e7
Madhusha <madhushahansini.perera@gmail.com>
2023-08-28 13:58:40 +0530

    push labsheet10

--- q2.scala
+++ q2.scala
@@ -1,11 +1,10 @@
 object q2 {
 
   def countLetterOccurrences(wordList: List[String]): Int = {
+
     val wordLengths = wordList.map(_.length)
 
-    // Use foldLeft to calculate the total letter count
     val letterCount = wordLengths.reduce((x, y) => x + y)
-
 
     letterCount
   }
@@ -13,13 +12,12 @@ object q2 {
   def main(args: Array[String]): Unit = {
     println("Enter the list of words separated by commas:")
 
-    // Split the input string into a list of strings
     val inputString = scala.io.StdIn.readLine()
     val inputList = inputString.split(",").toList
 
-    // Call the countLetterOccurrences function
     val totalLetterCount = countLetterOccurrences(inputList)
 
     println("Total count of letter occurrences: " + totalLetterCount)
+
   }
 }
